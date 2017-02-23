6f5e727d2c0cfcb4bf48c593a09706c5754cc7eb
James Pickering <james.pickering@xml-solutions.com>
2017-02-23 16:18:41 +0000

    Change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(name='ffx',
       install_requires=['pycrypto'],
       test_suite = 'ffx.tests',
-      version='0.0.1',
+      version='0.0.2',
       description='FFX',
       author='Kevin P. Dyer',
       author_email='kpdyer@gmail.com',
